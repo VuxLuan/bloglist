072cf787af5970f1232377ff47c755d5c742e601
VuxLuan <luanvu@MBL.local>
2025-08-22 09:36:13 +0700

    before pull

--- controllers/bloglist.js
+++ controllers/bloglist.js
@@ -4,30 +4,22 @@ const blogsRouter = express.Router();
 
 import Blog from "../models/blog.js";
 
-blogsRouter.get("/", async (request, response, next) => {
-  try {
-    const blogs = await Blog.find({});
-    response.json(blogs);
-  } catch (error) {
-    next(error);
-  }
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   });
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (error) {
-    next(error);
-  }
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 export default blogsRouter;
